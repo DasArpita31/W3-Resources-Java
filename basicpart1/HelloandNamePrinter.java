0b9b4b736adb4da4c7276c3e0d00dffd5881585d
Arpita Das <arpita1477@gmail.com>
2025-06-10 23:50:29 +0600

    01

---
+++ basicpart1/HelloandNamePrinter.java
@@ -0,0 +1,9 @@
+//1.Write a Java program to print 'Hello' on screen and your name on a separate line.
+package basicpart1;
+
+public class HelloandNamePrinter {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Alexandra Abramov");
+    }
+}
