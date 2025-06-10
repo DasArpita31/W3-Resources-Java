8e23ceadd00154862643f67a40deb869307d145d
Arpita Das <arpita1477@gmail.com>
2025-06-10 23:54:03 +0600

    02

---
+++ basicpart1/SumofTwoNumbers.java
@@ -0,0 +1,9 @@
+//Write a Java program to print the sum of two numbers.
+package basicpart1;
+
+public class SumofTwoNumbers {
+    public static void main(String[] args) {
+        int sum = 74+36;
+        System.out.println("Sum of two numbers = "+sum);
+    }
+}
