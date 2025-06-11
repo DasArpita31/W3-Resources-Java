4e7dbe72db56d987a466225cd307ae64160d4b9b
Arpita Das <arpita1477@gmail.com>
2025-06-11 10:15:04 +0600

    03

---
+++ basicpart1/DivisionofTwoNumbers.java
@@ -0,0 +1,10 @@
+//Write a Java program to divide two numbers and print them on the screen.
+package basicpart1;
+
+public class DivisionofTwoNumbers {
+    public static void main(String[] args) {
+
+        int result = 50/3;
+        System.out.println(result);
+    }
+}
