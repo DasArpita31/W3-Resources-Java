8f6371f50cdccaadc673ffb0a63daf2ac1dcbd53
Arpita Das <arpita1477@gmail.com>
2025-06-11 10:59:14 +0600

    09

---
+++ basicpart1/ExpressionEvaluation.java
@@ -0,0 +1,11 @@
+//Write a Java program to compute the specified expressions and print the output.
+
+package basicpart1;
+
+public class ExpressionEvaluation {
+
+    public static void main(String[] args) {
+        System.out.println(((25.5 * 3.5 - 3.5 * 3.5) / (40.5 - 4.5)));
+        
+    } 
+}
