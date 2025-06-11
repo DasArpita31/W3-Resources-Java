0642319e5be8f9e48476b932162e2f0794432392
Arpita Das <arpita1477@gmail.com>
2025-06-11 12:23:18 +0600

    14

---
+++ basicpart1/AmericanFlagDisplay.java
@@ -0,0 +1,23 @@
+//Write a Java program to print an American flag on the screen.
+
+package basicpart1;
+
+public class AmericanFlagDisplay {
+    public static void main(String[] args) {
+          System.out.println("* * * * * * ==================================");
+        System.out.println(" * * * * *  ==================================");
+        System.out.println("* * * * * * ==================================");
+        System.out.println(" * * * * *  ==================================");
+        System.out.println("* * * * * * ==================================");
+        System.out.println(" * * * * *  ==================================");
+        System.out.println("* * * * * * ==================================");
+        System.out.println(" * * * * *  ==================================");
+        System.out.println("* * * * * * ==================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+        System.out.println("==============================================");
+    }
+}
